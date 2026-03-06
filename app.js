c4a2aecfba76e5e18b4f724bc58bb1f0271a2dd3
pepefcardoso <pppfcardoso@gmail.com>
2026-03-06 09:41:22 -0300

    added tours get/post endpoints

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'Welcome to the Express server!', app: 'Roamora' });
+    .json({ status: 'success', results: tours.length, data: { tours: tours } });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'POST request received!' });
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = { ...req.body, id: newId };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      if (error) {
+        console.error('Error writing to file:', error);
+      }
+
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
 
 const port = process.env.PORT || 3000;
