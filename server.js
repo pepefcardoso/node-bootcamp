584839a798e69e599bdfe88957512e5334434337
pepefcardoso <pppfcardoso@gmail.com>
2026-03-10 09:46:56 -0300

    setup mongodb and mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({path: './.env'});
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {})
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
